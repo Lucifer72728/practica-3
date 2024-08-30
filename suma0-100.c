2d07a9f55bfcc5822de5e9ac0f34ba46bd61e476
Lucifer72728 <wicho.horadeaventura@gmail.com>
2024-08-30 14:49:17 -0600

    Create suma0-100.c

---
+++ suma0-100.c
@@ -0,0 +1,17 @@
+/* Quintero Cruz Luis Fernando L´Eglisse
+Practica 3 * programa suma de 0 a 100*/
+#include <stdio.h>
+
+int i;
+int suma = 0;
+
+int main ()
+{
+  for (i = 0; i <=100; i++)
+  {
+    suma = suma + i;
+   }
+    
+    printf("La suma de los numeros del 0 al 100 es: %d\n", suma);
+    return 0;
+ }
